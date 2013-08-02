090d43d01fe1bc264a9506e426960f187729cd54
Siraaj Khandkar <siraaj@khandkar.net>
2013-08-02 15:52:52 -0400

    Parsing insertions and deletions totals.

--- src/git-anal-punchcard.R
+++ src/git-anal-punchcard.R
@@ -17,17 +17,75 @@ ParseTimestamps <- function(timestamps) {
       )
 }
 
-ParseLines <- function(lines) {
-  # Example log line: "Tue Nov 6 21:28:48 2012 -0500|Jeff Lebowski"
-  line.comps <- strsplit(lines, "\\|")
+DashesToZeros <- function(v) {
+  ifelse(v == "-", 0, v)
+}
+
+MsgFlatten <- function(msg) {
+  len <- length(msg)
+  head <- msg[1]
+  if (len > 1) {
+    edits <- msg[2:len]
+    edits <- matrix(unlist(strsplit(edits, "\t")), ncol=3, byrow=TRUE)
+    insertions <- as.numeric(DashesToZeros(edits[,1]))
+    deletions  <- as.numeric(DashesToZeros(edits[,2]))
+    total.insertions <- sum(insertions)
+    total.deletions  <- sum(deletions)
+    paste(c(head, total.insertions, total.deletions), collapse="|")
+  }
+  else
+    paste(c(head, 0, 0), collapse="|")
+}
+
+ExtractMsgs <- function(lines) {
+  msg.start.indices <- grep("^[A-Z]", lines)
+  msg.range.indices <- list()
+  num.msgs  <- length(msg.start.indices)
+  num.lines <- length(lines)
+  for (i in 1:num.msgs) {
+    range <- (
+      if (i == num.msgs)
+        msg.start.indices[i]:num.lines
+      else
+        msg.start.indices[i]:(msg.start.indices[i+1] - 1)
+    )
+    msg.range.indices[[i]] <- range
+  }
+  msgs <- lapply(msg.range.indices, function(indices) lines[indices])
+  msgs <- lapply(msgs, MsgFlatten)
+  unlist(msgs)
+}
+
+ParseLog <- function(lines) {
+  # Example log lines:
+  #   Tue Nov 6 21:28:48 2012 -0500|Jeff Lebowski
+  #   0\t3\tsrc/foo_bar.erl
+  #   1\t4\tsrc/foo_baz.erl
+  #   14\t15\tREADME.md
+  #   Tue Nov 5 17:01:27 2012 -0700|Walter Sobchak
+  #   -\t-\tbin/blob
+  #   2\t4\tsrc/foo_year.erl
+  #
+  # Will be extracted as:
+  #   Tue Nov 6 21:28:48 2012 -0500|Jeff Lebowski|1|7
+  #   Tue Nov 5 17:01:27 2012 -0700|Walter Sobchak|2|4
+
+  number.of.msg.fields <- 4
+
+  msgs       <- ExtractMsgs(lines)
+  msg.fields <- strsplit(msgs, "\\|")
 
   # Drop lines with missing data (most likely names)
-  line.comps.lengths <- lapply(line.comps, length)
-  line.comps <- line.comps[line.comps.lengths == 2]
+  msg.comps.lengths <- lapply(msg.fields, length)
+  msg.fields <- msg.fields[msg.comps.lengths == number.of.msg.fields]
 
-  log.data  <- matrix(unlist(line.comps), ncol=2, byrow=T)
+  log.data  <- matrix(unlist(msg.fields), ncol=number.of.msg.fields, byrow=T)
   log.times <- ParseTimestamps(log.data[,1])
   log.names <- log.data[,2]
+
+  log.insertions <- log.data[,3]
+  log.deletions  <- log.data[,4]
+
   all.days  <- c("Sun", "Sat", "Fri", "Thu", "Wed", "Tue", "Mon")
   all.hours <- 0:23
   data.frame( Day  = factor(log.times$days , levels=all.days)
@@ -55,8 +113,11 @@ PlotPunchcard <- function(data, byname=FALSE) {
 Main <- function() {
   args <- commandArgs(trailingOnly=TRUE)
   n.top.committers <- if (length(args) > 0) args[1] else 0
-  log.lines <- system("git log --format='%ad|%an'", intern=TRUE)
-  log.data  <- ParseLines(log.lines)
+
+  log.cmd   <- "git log --format='%ad|%an' --numstat | grep -v '^$'"
+  log.lines <- system(log.cmd, intern=TRUE)
+  log.data  <- ParseLog(log.lines)
+
   punchcard.tbl  <- as.data.frame(table(log.data))
   punchcard.plot <- (
     if (n.top.committers > 0) {
